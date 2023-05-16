de53cfc1bc35d17cfae01388cf8a9d5a419ec91a
Brandon Roberts <robertsbt@gmail.com>
2023-05-16 17:22:03 -0500

    chore: remove build fixes

--- tools/paths.mjs
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-export const root = path.resolve(dirname, "..");
-export const client = path.resolve(root, "dist/analog/public");
-export const ssr = path.resolve(root, "dist/analog/server");
-export const cloudflare = path.resolve(root, "dist/cloudflare");
-export const worker = path.resolve(cloudflare, "_worker.js");
